9229097763174fd0d9334a75f6dc6173ce27843f
Nathan Nowack <thrast36@gmail.com>
2022-10-04 02:00:29 -0500

    refactor circuit breaker

--- prefect_monte_carlo/circuit_breakers.py
+++ prefect_monte_carlo/circuit_breakers.py
@@ -1,22 +1,45 @@
 """ Module for interacting with Monte Carlo circuit breakers / SQL monitor rules. """
-
 from typing import Optional
 from uuid import UUID
 
-from prefect import get_run_logger, task
+from prefect import flow, get_run_logger, task
+from prefect.orion.schemas.states import Completed
 from pycarlo.features.circuit_breakers import CircuitBreakerService
+from pycarlo.features.circuit_breakers.exceptions import CircuitBreakerPollException
 
 from prefect_monte_carlo.credentials import MonteCarloCredentials
 
 
+def circuit_breaker(**monitor_rule_kwargs):
+    """Decorator to run a parent flow whose children are only
+    run if a Monte Carlo SQL monitor rule is not breached."""
+
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            @flow
+            def circuit_breaker_flow(**monitor_rule_kwargs):
+                if monitor_rule_breached(**monitor_rule_kwargs):
+                    return Completed(
+                        message=f"Circuit breaker tripped for rule: {dict(**monitor_rule_kwargs)}."
+                    )
+                func(*args, **kwargs)
+
+            return circuit_breaker_flow(**monitor_rule_kwargs)
+
+        return wrapper
+
+    return decorator
+
+
 @task
-async def circuit_breaker(
+def monitor_rule_breached(
     monte_carlo_credentials: MonteCarloCredentials,
     rule_uuid: Optional[UUID] = None,
     rule_name: Optional[str] = None,
     namespace: Optional[str] = None,
+    timeout_in_minutes: Optional[int] = 5,
 ) -> bool:
-    """_summary_
+    """A task to check if a Monte Carlo circuit breaker / sql monitor rule is breached.
 
     Args:
         monte_carlo_credentials: The Monte Carlo credentials block used to
@@ -24,9 +47,38 @@ async def circuit_breaker(
         rule_uuid: UUID of the rule (custom SQL monitor) to execute.
         rule_name: Name of the rule (custom SQL monitor) to execute.
         namespace: Namespace of the rule (custom SQL monitor) to execute.
+        timeout_in_minutes: How long to wait for SQL rule run logs.
 
     Raises:
         ValueError: If an incorrect combination of rule references is passed.
+
+    Examples:
+        - Check if a rule is breached by UUID:
+        ```python
+        from prefect import flow
+        from prefect_monte_carlo import monitor_rule_breached
+
+        @task
+        def do_something_if_not_breached():
+            pass
+
+        @flow
+        def test(we_really_care_about_UUID: bool = False):
+            # we can also pass `rule_name` and `namespace` instead of rule_uuid
+            rule_uuid = (
+                UUID("af6fdb62-7496-4b8b-ba09-38f83a311c17") if we_really_care_about_UUID
+                else "af6fdb62-7496-4b8b-ba09-38f83a311c17" # just a str will do
+            )
+            if monitor_rule_breached(
+                monte_carlo_credentials=MonteCarloCredentials.load(
+                    "monte-carlo-credentials"
+                ),
+                rule_uuid=rule_uuid
+            ):
+                return Completed(message="SQL Monitor rule breached - skipping flow run.")
+
+            do_something_if_not_breached()
+            ```
     """
     monte_carlo_client = monte_carlo_credentials.get_client()
 
@@ -37,15 +89,34 @@ async def circuit_breaker(
         raise ValueError("Must pass either `rule_uuid` OR `rule_name` and `namespace`.")
 
     if rule_uuid:
-        rule_reference = dict(rule_uuid=rule_uuid)
+        circuit_breaker_svc_kwargs = dict(rule_uuid=rule_uuid)
     else:
-        rule_reference = dict(rule_name=rule_name, namespace=namespace)
+        circuit_breaker_svc_kwargs = dict(rule_name=rule_name, namespace=namespace)
 
     logger = get_run_logger()
 
-    circuit_breaker = CircuitBreakerService(
+    circuit_breaker_service = CircuitBreakerService(
         mc_client=monte_carlo_client, print_func=logger.info
     )
 
-    if circuit_breaker.trigger_and_poll(**rule_reference):
-        logger.info("Circuit breaker triggered")
+    job_execution_uuid = circuit_breaker_service.trigger(**circuit_breaker_svc_kwargs)
+
+    logger.debug(
+        f"Triggered rule: {rule_uuid} with job execution UUID: {job_execution_uuid}"
+    )
+
+    try:
+        N_breaches = circuit_breaker_service.poll(
+            job_execution_uuid=job_execution_uuid, timeout_in_minutes=timeout_in_minutes
+        )
+
+        if N_breaches > 0:
+            logger.info(f"Found {N_breaches} breach(es) of this rule.")
+            return True
+
+        logger.info("Found no breaches of rule.")
+        return False
+
+    except CircuitBreakerPollException as e:
+        logger.error(f"{e!r}")
+        raise
--- tests/test_circuit_breakers.py
+++ tests/test_circuit_breakers.py
@@ -2,7 +2,7 @@ from uuid import UUID
 
 from prefect import flow
 
-from prefect_monte_carlo.circuit_breakers import circuit_breaker
+from prefect_monte_carlo.circuit_breakers import monitor_rule_breached
 from prefect_monte_carlo.credentials import MonteCarloCredentials
 
 
@@ -10,10 +10,14 @@ async def test_circuit_breaker_with_uuid(monte_carlo_credentials):
     """Test that the circuit breaker task can be run with a rule UUID."""
 
     @flow
-    def test():
-        rule_uuid = UUID("af6fdb62-7496-4b8b-ba09-38f83a311c17")
-
-        return circuit_breaker(
+    def test(we_really_care_about_UUID: bool = False):
+        # we can also pass both `rule_name` and `namespace` instead of rule_uuid
+        rule_uuid = (
+            UUID("af6fdb62-7496-4b8b-ba09-38f83a311c17")
+            if we_really_care_about_UUID
+            else "af6fdb62-7496-4b8b-ba09-38f83a311c17"  # just a str will do
+        )
+        return monitor_rule_breached(
             monte_carlo_credentials=MonteCarloCredentials.load(
                 "monte-carlo-credentials"
             ),
@@ -23,3 +27,16 @@ async def test_circuit_breaker_with_uuid(monte_carlo_credentials):
     result = test()
 
     assert isinstance(result, bool)
+
+
+# if __name__ == "__main__":
+#     @circuit_breaker(
+#         rule_uuid="af6fdb62-7496-4b8b-ba09-38f83a311c17",
+#         monte_carlo_credentials=MonteCarloCredentials.load("monte-carlo-credentials")
+#     )
+#     @flow
+#     def only_run_if_not_breached(shabba = "ranks"):
+#         logger = get_run_logger()
+#         logger.info(f"shabba {shabba}")
+
+#     only_run_if_not_breached()
