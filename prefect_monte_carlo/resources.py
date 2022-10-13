c95b607c370157e8271ac2097d53a5dadad2d329
Nathan Nowack <thrast36@gmail.com>
2022-10-13 16:01:20 -0500

    add detail to `get_monte_carlo_resources` docstring

--- prefect_monte_carlo/resources.py
+++ prefect_monte_carlo/resources.py
@@ -11,13 +11,15 @@ async def get_monte_carlo_resources(
     monte_carlo_credentials: MonteCarloCredentials,
 ) -> box.BoxList:
     """
-    Task to retrieve all Monte Carlo resources.
+    Task to retrieve aggregate Monte Carlo resource information via the `getResources`
+    GraphQL query. Fields selected by this query include: `name`, `type`, `id`, `uuid`,
+    `isDefault`, and `isUserProvided`.
 
     Args:
         monte_carlo_credentials: The Monte Carlo credentials block used to generate
             an authenticated GraphQL API client via pycarlo.
     Returns:
-        A `box.BoxList` of all Monte Carlo resources.
+        A `box.BoxList` of Monte Carlo resources.
     """
 
     client = monte_carlo_credentials.get_client()
